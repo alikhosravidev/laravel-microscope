0056e084649cd15eab3c3ed804d81bf4b6ede60c
Iman <imanghafoori1@gmail.com>
2025-09-03 02:02:18 +0330

    refactor

--- src/Features/CheckDD/CheckDD.php
+++ src/Features/CheckDD/CheckDD.php
@@ -11,15 +11,17 @@ class CheckDD implements Check
 {
     use CachedCheck;
 
+    public static $onErrorCallback;
+
     /**
      * @var string
      */
     private static $cacheKey = 'check_dd_command';
 
-    private static function performCheck(PhpFileDescriptor $file, $params): bool
+    private static function performCheck(PhpFileDescriptor $file): bool
     {
         $tokens = $file->getTokens();
-        $callback = $params[0];
+        $callback = self::$onErrorCallback;
         $hasError = false;
         foreach ($tokens as $i => $token) {
             if (($index = FunctionCall::isGlobalCall('dd', $tokens, $i)) || ($index = FunctionCall::isGlobalCall('dump', $tokens, $i)) || ($index = FunctionCall::isGlobalCall('ddd', $tokens, $i))) {
